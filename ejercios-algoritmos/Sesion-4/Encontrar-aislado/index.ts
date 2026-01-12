2e50debcc8734ba187cc12874c7688516d5910a1
Avila-Pato <p.avilaf1998@gmail.com>
2026-01-11 21:45:34 -0300

    Retornando aislado

---
+++ ejercios-algoritmos/Sesion-4/Encontrar-aislado/index.ts
@@ -0,0 +1,19 @@
+// Encontrar al aislado
+// Dado un array de números enteros, pares o impares, de al menos 3 elementos, donde todos los elementos son pares o todos los elementos son impares,
+//  excepto uno. Escribir una función que tome como parametro el array y devuelva el elemento aislado.
+
+// [2, 4, 0, 100, 4, 11, 2602, 36]  =>  11
+
+// [160, 3, 1719, 19, 11, 13, -21]  =>  160
+
+function Aislado() {
+  const array = [2, 4, 0, 100, 4, 11, 2602, 36];
+
+  if (array.length % 2 === 0) {
+    return array.filter((num) => num % 2 !== 0)[0];
+  } else {
+    return "odd";
+  }
+}
+
+console.log(Aislado());
