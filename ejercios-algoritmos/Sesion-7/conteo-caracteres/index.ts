4f993ed14c715767871869328606bbece9a660e6
Avila-Pato <p.avilaf1998@gmail.com>
2026-01-20 13:10:17 -0300

    Conteo caracteres

--- ejercios-algoritmos/Sesion-7/conteo-caracteres/index.ts
+++ ejercios-algoritmos/Sesion-7/conteo-caracteres/index.ts
@@ -1,11 +1,24 @@
 // Conteo de caracteres
-// Implementá una función que reciba un string y devuelva un objeto con el conteo de cada carácter. La función debe ser case-sensitive (distinguir mayúsculas de minúsculas) y contar todos los caracteres incluyendo espacios.
+// Implementá una función que reciba un string y devuelva un objeto con el conteo de cada carácter. 
+// La función debe ser case-sensitive (distinguir mayúsculas de minúsculas) y contar todos los caracteres 
+// incluyendo espacios.
 
 // conteoCaracteres("hello"); 
-// // { h: 1, e: 1, l: 2, o: 1 }
+// { h: 1, e: 1, l: 2, o: 1 }
 
 // conteoCaracteres("Hello World!");
-// // { H: 1, e: 1, l: 3, o: 2, ' ': 1, W: 1, r: 1, d: 1, '!': 1 }
+// { H: 1, e: 1, l: 3, o: 2, ' ': 1, W: 1, r: 1, d: 1, '!': 1 }
 
 // conteoCaracteres("");
-// // {}
+
+function conteoCaracteres(palabra: string) {
+    const conteoCaracteres: Record<string, number> = {}
+
+    for (const letra of palabra) {
+        conteoCaracteres[letra] = (conteoCaracteres[letra] || 0) + 1
+    }       
+    return conteoCaracteres
+}   
+
+console.log(conteoCaracteres("hello"))
+console.log(conteoCaracteres("Hello World!"))
