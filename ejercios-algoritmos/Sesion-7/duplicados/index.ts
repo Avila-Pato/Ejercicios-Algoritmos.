423b557677d3116cc1bfb415d123f7d9cf8b79aa
Avila-Pato <p.avilaf1998@gmail.com>
2026-01-20 13:54:35 -0300

    // new Set(array).size es el array nuevo sin duplicados // array.length es el array original

--- ejercios-algoritmos/Sesion-7/duplicados/index.ts
+++ ejercios-algoritmos/Sesion-7/duplicados/index.ts
@@ -1,8 +1,18 @@
 // Verificar duplicados
-// Implementá una función que reciba un array y devuelva true si tiene elementos repetidos o false si no los tiene.
+// Implementá una función que reciba un array y devuelva true si tiene elementos repetidos
+//  o false si no los tiene.
 
 // tieneDuplicados([1, 2, 2, 3, 4, 4, 5]); // true
 // tieneDuplicados([5, 5, 5, 5]); // true
 // tieneDuplicados([1, 2, 3]); // false
 // tieneDuplicados([]); // false
 // tieneDuplicados([7, 3, 8, 3, 1, 9]); // true
+function Duplicados(array: any) {
+
+return new Set(array).size !== array.length;
+// new Set(array).size es el array nuevo sin duplicados
+// array.length es el array original 
+    
+}
+console.log(Duplicados([1, 2, 2, 3, 4, 4, 5]))
+console.log(Duplicados([1,2,3]))
